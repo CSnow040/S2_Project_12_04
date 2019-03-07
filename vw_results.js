7c73fea24aeb0ef2660cd94911f0d774cf0ccc93
Unknown <CSnow040@west-mec.org>
2019-03-06 21:15:56 -0700

    Final Commit
    Commented all the code I worked on out to be completely finished and ready to be graded

--- vw_results.js
+++ vw_results.js
@@ -23,21 +23,21 @@
    
       
 */
-
+// This variable simply calls upon the text string holding the race title and amking it a value for every race to have their specific title
 var reportHTML = "<h1>" + raceTitle + "</h1>";
-
+//This main for loop makes it so that all of the votes wuithin the specific races have their specific amounts of votes going to whatever candidate using the table tag with table rows with table headers
 for (var i = 0; i < race.length; i++) {
     var totalVotes = 0;
     votes[i].forEach(calcSum);
     reportHTML += "<table>";
     reportHTML += "<caption>" + race[i] + "</caption>";
-    reportHTML += "<tr><th>Candidate</th><th>Votes</th<tr>";
+    reportHTML += "<tr><th>Candidate</th><th>Votes</th><tr>";
     reportHTML += candidateRows(i, totalVotes);
     reportHTML += "</table>";
 }
-
+//The document here puts all of the following JavaScript into the section tag that we have placed specifically into the index.html file 
 document.getElementsByTagName("section")[0].innerHTML = reportHTML;
-
+//This function makes all of the specific candidate variables for each candidate in each race including their name, party, votes that is later presented by the bar function
 function candidateRows(raceNum, totalVotes) {
     var rowHTML = "";
     for (var j = 0; j <= 2; j++) {
@@ -46,13 +46,14 @@ function candidateRows(raceNum, totalVotes) {
         var candidateVotes = votes[raceNum][j];
         var candidatePercent = calcPercent(candidateVotes, totalVotes);
         rowHTML += "<tr><td>" + candidateName + "(" + candidateParty + ")</td><td>" + candidateVotes.toLocaleString() + "(" + candidatePercent.toFixed(1) + "%)</td></tr>"
-
+//This nested for loop actually makes the physical look of the bar itself
         for (var k = 0; k < candidatePercent; k++) {
             rowHTML += createBar(candidateParty);
         }
-
+//This is the ending table row tag to keep in track with the rowHTML variable and whewre it ends
         rowHTML += "</tr>"
     }
+//This return value makes an ending point for the row HTML's effects 
     return rowHTML;
 }
 
@@ -65,7 +66,7 @@ function calcSum(value) {
 function calcPercent(value, sum) {
     return (100 * value / sum);
 }
-
+//This function holds the switch case which makes it so that each race will have visual stats for the user to see and compare even
 function createBar(partyType) {
     var barHTML = "";
     switch (partyType) {
