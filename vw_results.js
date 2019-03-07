6634c36d26f9fedc843865b94559729a2751c7f0
Unknown <CSnow040@west-mec.org>
2019-03-06 17:23:10 -0700

    Finished JavaScript
    Done with all JavaScript just need to comment it all out

--- vw_results.js
+++ vw_results.js
@@ -26,14 +26,35 @@
 
 var reportHTML = "<h1>" + raceTitle + "</h1>";
 
-for (var i = race; i++;) {
+for (var i = 0; i < race.length; i++) {
     var totalVotes = 0;
     votes[i].forEach(calcSum);
-    reportHTML += "<table><caption>" + race[i] + "</caption> <tr><th>Candidate</th><th>Votes</th<tr>";
-    reportHTML += candidateRows();
+    reportHTML += "<table>";
+    reportHTML += "<caption>" + race[i] + "</caption>";
+    reportHTML += "<tr><th>Candidate</th><th>Votes</th<tr>";
+    reportHTML += candidateRows(i, totalVotes);
+    reportHTML += "</table>";
 }
 
+document.getElementsByTagName("section")[0].innerHTML = reportHTML;
 
+function candidateRows(raceNum, totalVotes) {
+    var rowHTML = "";
+    for (var j = 0; j <= 2; j++) {
+        var candidateName = candidate[raceNum][j];
+        var candidateParty = party[raceNum][j];
+        var candidateVotes = votes[raceNum][j];
+        var candidatePercent = calcPercent(candidateVotes, totalVotes);
+        rowHTML += "<tr><td>" + candidateName + "(" + candidateParty + ")</td><td>" + candidateVotes.toLocaleString() + "(" + candidatePercent.toFixed(1) + "%)</td></tr>"
+
+        for (var k = 0; k < candidatePercent; k++) {
+            rowHTML += createBar(candidateParty);
+        }
+
+        rowHTML += "</tr>"
+    }
+    return rowHTML;
+}
 
 /* Callback Function to calculate an array sum */
 function calcSum(value) {
@@ -43,4 +64,20 @@ function calcSum(value) {
 /* Function to calculate a percentage */
 function calcPercent(value, sum) {
     return (100 * value / sum);
+}
+
+function createBar(partyType) {
+    var barHTML = "";
+    switch (partyType) {
+        case "D":
+            barHTML = "<td class='dem'></td>"
+            break;
+        case "R":
+            barHTML = "<td class='rep'></td>"
+            break;
+        case "I":
+            barHTML = "<td class='ind'></td>"
+            break;
+    }
+    return barHTML;
 }
